eced709a3d3a1f6fe125d56c2caa0e8b9375024d
Yujun Oh <34445892+ohurjon@users.noreply.github.com>
2020-07-10 00:38:45 +0900

    자가진단 v0.1
    귀찮아서 만들었습니다. 하지만 분석하는게 더 귀찮았습니다.

---
+++ Main.js
@@ -0,0 +1,19 @@
+var {CronJob} = require('cron');
+const got = require('got');
+const moment = require('moment');
+site = 'https://eduro.cbe.go.kr/stv_cvd_co01_000.do?rtnRsltCode=SUCCESS&qstnCrtfcNoEncpt=(자신의 개인 인증 Token 아직 자동화가 이루어지지 않은 부분입니다.)&rspns01=1&rspns02=1&rspns07=0&rspns08=0&rspns09=0'
+console.log("==============================================")
+console.log("\n\n매 7시 30분 마다 코드가 반복됩니다 (주말 제외)\n\n")
+console.log("==============================================\n\n [로그] \n")
+async function JGGD(){
+    const get = await got.post(site);
+    data = JSON.parse(get.body);
+        if(data.resultSVO.rtnRsltCode == "SUCCESS"){
+            d = moment(get.req.timings.end).tz('Asia/Seoul');
+            console.log(`성공적으로 자가진단을 완료 했습니다. ${data.resultSVO.schulNm} ${data.resultSVO.stdntName} ${d}`);
+        } else  {
+            console.log(get);
+        }
+}
+job = new CronJob('00 30 7 * * 1-5', JGGD() ,null,true,'Asia/Seoul');
+job.start();
